9cb6fb5ee4fe874f9b07f9420c5153183aee0477
MiniLifeHihi <M1n1l1f3h!H!>
2024-10-06 08:52:27 +0700

    update

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images:{
+      unoptimized:true
+    }
+    // ...các cấu hình khác
+  };
+  
+  module.exports = nextConfig;
